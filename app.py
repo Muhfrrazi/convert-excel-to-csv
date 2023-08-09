5cff8d3c0080c45425616481a31ba452f773a765
Muhammad Fachrurrazi <mfachrurrazi41@gmail.com>
2023-08-10 07:52:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def process_excel(excel_file, excel_file_name):
     output_file_name = os.path.splitext(excel_file_name)[0] + '_output.xlsx'
     output_file_path = os.path.join('temp', output_file_name)
     df.to_excel(output_file_path, index=False)
+    
     return output_file_name
 
 st.title('File Converter')
@@ -53,50 +54,37 @@ if csv_files:
     st.write('Conversion Log:')
     for file_path in output_excel_files:
         st.write(file_path)
-
-    # Tambahkan tombol unduh dengan tipe file Excel (xlsx) yang sesuai
-    for output_file_path in output_excel_files:
-        if os.path.exists(output_file_path):
-            output_file_name = os.path.basename(output_file_path)
-            st.download_button(f"Download {output_file_name}", output_file_path, file_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
+        st.download_button(f"Download {os.path.basename(file_path)}", file_path, file_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
 output_excel_names = []  # Initialize the list before the conditional block
 conversion_history = []
 
-# Setelah loop yang memproses file Excel
 if excel_files:
-    output_excel_files = []  # Initialize list to store output Excel file paths
-    
     for excel_file in excel_files:
         output_file_name = process_excel(excel_file, excel_file.name)
-        output_excel_files.append(output_file_name)
-    
-    st.write('Conversion Log:')
-    for file_name in output_excel_files:
-        st.write(file_name)
+        output_path = os.path.join('temp', output_file_name)
+        output_excel_files.append(output_path)
+        st.write('Conversion Log:')
+        st.write(output_file_name)
+        # Tambahkan tombol unduh dengan tipe file Excel (xlsx) yang sesuai
+        st.download_button(f"Download {output_file_name}", output_path, file_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
-    # Tambahkan tombol unduh dengan tipe file Excel (xlsx) yang sesuai
-    # Inside the loop for processing Excel files
-for output_file_name in output_excel_files:
-    output_path = os.path.join('temp', output_file_name)
-    if os.path.exists(output_path):
-        output_file_name = os.path.basename(output_file_path)
-        st.download_button(
-            label=f"Download {output_file_name}",
-            data=output_path,
-            file_name=output_file_name)
-
-
-# After the loops, you can use the conversion history
 st.write('Conversion Log:')
-for item in conversion_history:
+for item in output_excel_files:
     st.write(item)
 
 if st.button("Clear Log"):
-    conversion_history.clear()
+    output_excel_files.clear()
 
-for item in conversion_history:
+for item in output_excel_files:
     st.write(item)
+if st.button("Print History to Word"):
+    doc = Document()
+    doc.add_heading('Conversion History', level=1)
 
-st.button("Print History to Word")
+    for item in conversion_history:
+        doc.add_paragraph(item)
+
+    word_filename = 'conversion_history.docx'
+    doc.save(word_filename)
+    st.success(f"Conversion history saved as '{word_filename}'")
