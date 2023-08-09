67a723aeac360af2b4dea594cbca1b811eea423d
Muhammad Fachrurrazi <mfachrurrazi41@gmail.com>
2023-08-09 22:16:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ def process_files(csv_files):
         if 'NO;"NPWP15";"Nama_WP"' in df.columns:
             df['NO;"NPWP15";"Nama_WP"'] = df['NO;"NPWP15";"Nama_WP"'].str.replace('"', '')
 
-        excel_file = csv_file.replace('.csv', '.xlsx')
-
-        if os.path.exists(excel_file):
-            st.warning(f"{excel_file} already exists. Overwriting it.")
-        df.to_excel(excel_file, index=False)
-        process_excel(excel_file)
+        excel_file_name = os.path.splitext(csv_file.name)[0] + '.xlsx'
+        excel_file_path = os.path.join('temp', excel_file_name)
+        
+        if os.path.exists(excel_file_path):
+            st.warning(f"{excel_file_name} already exists. Overwriting it.")
+        df.to_excel(excel_file_path, index=False)
+        process_excel(excel_file_path)
 
 def process_excel_to_csv(excel_files):
     dfs = []
@@ -23,13 +24,14 @@ def process_excel_to_csv(excel_files):
         dfs.append(df)
 
     combined_df = pd.concat(dfs, ignore_index=True)
-    output_file_csv = excel_files[0].replace('.xlsx', '_combined_output.csv')
+    output_file_csv_name = os.path.splitext(excel_files[0].name)[0] + '_combined_output.csv'
+    output_file_csv_path = os.path.join('temp', output_file_csv_name)
 
-    if os.path.exists(output_file_csv):
-        st.warning(f"{output_file_csv} already exists. Overwriting it.")
-    combined_df.to_csv(output_file_csv, index=False, header=True)
-    output_label.text(f'Converted to CSV: {output_file_csv}')
-    history_listbox.write(output_file_csv)
+    if os.path.exists(output_file_csv_path):
+        st.warning(f"{output_file_csv_name} already exists. Overwriting it.")
+    combined_df.to_csv(output_file_csv_path, index=False, header=True)
+    output_label.text(f'Converted to CSV: {output_file_csv_name}')
+    history_listbox.write(output_file_csv_name)
 
 def process_excel(excel_file):
     df = pd.read_excel(excel_file)
@@ -40,11 +42,14 @@ def process_excel(excel_file):
         st.warning("Column 'NO;NPWP15;Nama_WP' not found in the DataFrame.")
 
     df.drop(columns=['NO;"NPWP15";"Nama_WP"'], inplace=True)
-    output_file = excel_file.replace('.xlsx', '_output.xlsx')
-    df.to_excel(output_file, index=False)
-    output_label.text(f'Output File: {output_file}')
+    output_file_name = os.path.splitext(excel_file.name)[0] + '_output.xlsx'
+    output_file_path = os.path.join('temp', output_file_name)
+    df.to_excel(output_file_path, index=False)
+    output_label.text(f'Output File: {output_file_name}')
 
 st.title('File Converter')
+
+os.makedirs('temp', exist_ok=True)
 
 csv_files = st.file_uploader("Choose CSV Files", type="csv", accept_multiple_files=True)
 excel_files = st.file_uploader("Choose Excel Files", type="xlsx", accept_multiple_files=True)
